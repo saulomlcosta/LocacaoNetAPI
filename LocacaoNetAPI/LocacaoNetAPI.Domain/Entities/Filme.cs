33e4f70b469d16f322e53d1b1aef173dca310dea
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-06-29 16:31:06 -0300

    Criação das Entidades

---
+++ LocacaoNetAPI/LocacaoNetAPI.Domain/Entities/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocacaoNetAPI.Domain.Entities
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int ClassificacaoIndicativa { get; set; }
+        public byte Lancamento { get; set; }
+    }
+}
---
+++ LocacaoNetAPI/LocacaoNetAPI.Domain/Entities/Locacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocacaoNetAPI.Domain.Entities
+{
+    public class Locacao
+    {
+        public int Id { get; set; }
+        public Cliente Cliente { get; set; }
+        public int Id_Cliente { get; set; }
+        public Filme Filme { get; set; }
+        public int Id_Filme { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public DateTime DataDevolucao { get; set; }
+    }
+}
