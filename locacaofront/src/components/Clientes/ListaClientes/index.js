0acc615e2591f70323e9babed9b597d25d34ab1d
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-01-22 19:14:26 -0300

    Identation and Fix Loop Eternal on api/clientes

--- locacaofront/src/components/Clientes/ListaClientes/index.js
+++ locacaofront/src/components/Clientes/ListaClientes/index.js
@@ -1,110 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import { Button, Table, Container, Row, Col } from 'react-bootstrap';
-import { Modal } from 'react-bootstrap';
-import { FiEdit, FiUserX } from 'react-icons/fi'
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Button, Table, Container, Row, Col } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
+import { FiEdit, FiUserX } from "react-icons/fi";
 
-
-import api from '../../../services/api';
-
+import api from "../../../services/api";
 
 export default function Clientes() {
-    const [clientes, setClientes] = useState([]);
-    const [clienteSelecionado, setClienteSelecionado] = useState();
+  const [clientes, setClientes] = useState([]);
+  const [clienteSelecionado, setClienteSelecionado] = useState();
 
-    const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(false);
 
-    const fecharModal = () => setModal(false);
+  const fecharModal = () => setModal(false);
 
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  async function carregarClientes() {
+    try {
+      var response = await api.get(`api/clientes`);
 
-    async function carregarClientes() {
-        try {
-            var response = await api.get(`api/clientes`);
-
-            setClientes(response.data);
-            carregarClientes();
-        } catch (error) {
-            alert('Não foi possível carregar os clientes');
-        }
+      setClientes(response.data);
+    } catch (error) {
+      alert("Não foi possível carregar os clientes");
     }
+  }
 
-    async function editarCliente(id) {
-        try {
-            navigate(`/clientes/novo/${id}`)
-        } catch (error) {
-            alert('Não foi possível ir para tela de edição');
-        }
+  async function editarCliente(id) {
+    try {
+      navigate(`/clientes/novo/${id}`);
+    } catch (error) {
+      alert("Não foi possível ir para tela de edição");
     }
+  }
 
-    async function excluirCliente(id) {
-        try {
-            await api.delete(`api/clientes/${id}`);
-            setModal(false);
-
-        } catch (error) {
-            alert('Não foi possível deletar o cliente');
-        }
+  async function excluirCliente(id) {
+    try {
+      await api.delete(`api/clientes/${id}`);
+      setModal(false);
+    } catch (error) {
+      alert("Não foi possível deletar o cliente");
     }
+  }
 
-    async function selecionarCliente(cliente) {
-        setClienteSelecionado(cliente);
-        setModal(true);
-    }
+  async function selecionarCliente(cliente) {
+    setClienteSelecionado(cliente);
+    setModal(true);
+  }
 
+  useEffect(() => {
+    carregarClientes();
+  }, []);
 
-    useEffect(() => {
-        carregarClientes();
-    }, [])
-
-
-    return (
-        <>
-            <Container>
-                <Row>
-                    <Col><Button className="mb-3 mr-3" href="/">Voltar</Button></Col>
-                    <Col><Button className="mb-3" href="/clientes/novo/0">Criar Novo Cliente</Button></Col>
-                </Row>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>CPF</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {clientes.map(cliente => (
-
-                            <tr>
-                                <td>{cliente.nome}</td>
-                                <td>{cliente.cpf}</td>
-                                <td><Button variant="info" onClick={() => editarCliente(cliente.id)}>
-                                    <FiEdit size={25} color="#17202a" />
-                                </Button>
-                                </td>
-                                <td><Button variant="danger" onClick={() => selecionarCliente(cliente)}>
-                                    <FiUserX size={25} color="#17202a" />
-                                </Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-                <Modal show={modal} onHide={fecharModal}>
-                    <Modal.Body>Você confirma a exclusão deste(a) cliente? : {clienteSelecionado && clienteSelecionado.nome}</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={fecharModal}>
-                            Não
-                        </Button>
-                        <Button variant="primary" onClick={() => excluirCliente(clienteSelecionado.id)}>
-                            Sim
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Button className="mb-3 mr-3" href="/">
+              Voltar
+            </Button>
+          </Col>
+          <Col>
+            <Button className="mb-3" href="/clientes/novo/0">
+              Criar Novo Cliente
+            </Button>
+          </Col>
+        </Row>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>CPF</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <tr>
+                <td>{cliente.nome}</td>
+                <td>{cliente.cpf}</td>
+                <td>
+                  <Button
+                    variant="info"
+                    onClick={() => editarCliente(cliente.id)}
+                  >
+                    <FiEdit size={25} color="#17202a" />
+                  </Button>
+                </td>
+                <td>
+                  <Button
+                    variant="danger"
+                    onClick={() => selecionarCliente(cliente)}
+                  >
+                    <FiUserX size={25} color="#17202a" />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Modal show={modal} onHide={fecharModal}>
+          <Modal.Body>
+            Você confirma a exclusão deste(a) cliente? :{" "}
+            {clienteSelecionado && clienteSelecionado.nome}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={fecharModal}>
+              Não
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => excluirCliente(clienteSelecionado.id)}
+            >
+              Sim
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    </>
+  );
 }
